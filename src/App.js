6c5f227ce2d3d66747fb537602d513512d5aac02
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-02 00:28:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+// import { useState, useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import shortid from 'shortid';
+import store from './redux/store';
+// import shortid from 'shortid';
 import Form from './Components/Form/Form';
 import ContactList from './Components/ContactList/ContactList';
 import Filter from './Components/Filter/Filter';
@@ -9,74 +10,34 @@ import Title from './Components/Title/Title';
 import Container from './Components/Container/Container';
 
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
-  });
-  const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useState(() => {
+  //   return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  // });
+  // const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const formSubmitHandler = (name, number) => {
-    const contact = {
-      id: shortid.generate(),
-      name,
-      number,
-    };
-
-    if (name === '' || number === '') {
-      toast.info('Please fill all fields');
-      return;
-    } else if (
-      contacts.find(
-        contact => contact.name.toLowerCase() === name.toLowerCase(),
-      )
-    ) {
-      toast.info(`Name ${name} is already exsist`);
-      return;
-    } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(number)) {
-      toast.error('Enter the correct  phone number');
-      return;
-    }
-    setContacts(prevContacts => [contact, ...prevContacts]);
-  };
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
-    );
-  };
-
-  const deleteContact = contactId => {
-    setContacts(contacts.filter(({ id }) => id !== contactId));
-  };
-
-  const changeFilter = e => {
-    setFilter(e.currentTarget.value);
-  };
-
-  const visibleContacts = getVisibleContacts();
+  // useEffect(() => {
+  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
+  const state = store.getState();
+  console.log(state);
 
   return (
     <Container>
       <ToastContainer autoClose={3000} />
       <Title title="Phonebook" />
 
-      <Form onSubmit={formSubmitHandler} />
+      <Form />
 
       <Title title="Contacts" />
-      {contacts.length > 1 && <Filter value={filter} onChange={changeFilter} />}
-      {contacts.length > 0 ? (
-        <ContactList
-          contacts={visibleContacts}
-          onDeleteContact={deleteContact}
-        />
+      <Filter />
+      {/* {state.contacts.items.length > 1 && <Filter />} */}
+
+      <ContactList />
+      {/* {state.contacts.items.length > 0 ? (
+        <ContactList />
       ) : (
         <p>Add your first contact</p>
-      )}
+      )} */}
     </Container>
   );
 }
