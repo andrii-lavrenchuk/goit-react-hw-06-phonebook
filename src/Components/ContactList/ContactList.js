9e47c1ce63b7875092433ca546102d513cc402ce
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-02 00:28:29 +0200

    Update ContactList.js

--- src/Components/ContactList/ContactList.js
+++ src/Components/ContactList/ContactList.js
@@ -1,8 +1,9 @@
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import s from './ContactList.module.css';
+import contactActions from '../../redux/contacts/contacts-actions';
 import Contact from '../Contact/Contact';
 
-export default function ContactList({ contacts, onDeleteContact }) {
+function ContactList({ contacts, onDeleteContact }) {
   return (
     <ul className={s.list}>
       {contacts.map(({ id, name, number }) => (
@@ -18,13 +19,20 @@ export default function ContactList({ contacts, onDeleteContact }) {
   );
 }
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    }),
-  ),
-  onDeleteContact: PropTypes.func.isRequired,
+const getVisibleContacts = (allContacts, filter) => {
+  const normalizedFilter = filter.toLowerCase();
+
+  return allContacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter),
+  );
 };
+
+const mapStateToProps = ({ contacts: { items, filter } }) => ({
+  contacts: getVisibleContacts(items, filter),
+});
+
+const mapDispatchToProps = dispatch => ({
+  onDeleteContact: id => dispatch(contactActions.deleteContact(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
