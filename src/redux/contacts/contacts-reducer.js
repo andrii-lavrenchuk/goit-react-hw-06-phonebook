6dfcc2422dcf2fdb1f232fc663052d47705b54ba
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-02 19:39:22 +0200

    Update contacts-reducer.js

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,46 +1,55 @@
 import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
+import actions from '../contacts/contacts-actions';
 
-import types from './contacts-types';
+const items = createReducer([], {
+  [actions.addContact]: (state, { payload }) => [...state, payload],
+  [actions.deleteContact]: (state, { payload }) =>
+    state.filter(({ id }) => id !== payload),
+});
 
-const items = (state = [], { type, payload }) => {
-  switch (type) {
-    case types.ADD:
-      if (payload.name === '' || payload.number === '') {
-        toast.info('Please fill all fields');
-        return state;
-      } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(payload.number)) {
-        toast.error('Enter the correct  phone number');
-        return state;
-      } else if (
-        state.find(
-          item => item.name.toLowerCase() === payload.name.toLowerCase(),
-        )
-      ) {
-        toast.info(`Name ${payload.name} is already exsist`);
-        return state;
-      }
-      return [...state, payload];
-
-    case types.DELETE:
-      return state.filter(({ id }) => id !== payload);
-
-    default:
-      return state;
-  }
-};
-
-const filter = (state = '', { type, payload }) => {
-  switch (type) {
-    case types.CHANGE_FILTER:
-      return payload;
-
-    default:
-      return state;
-  }
-};
+const filter = createReducer('', {
+  [actions.changeFilter]: (_, { payload }) => payload,
+});
 
 export default combineReducers({
   items,
   filter,
 });
+// const items = (state = [], { type, payload }) => {
+//   switch (type) {
+//     case types.ADD:
+//       if (payload.name === '' || payload.number === '') {
+//         toast.info('Please fill all fields');
+//         return state;
+//       } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(payload.number)) {
+//         toast.error('Enter the correct  phone number');
+//         return state;
+//       } else if (
+//         state.find(
+//           item => item.name.toLowerCase() === payload.name.toLowerCase(),
+//         )
+//       ) {
+//         toast.info(`Name ${payload.name} is already exsist`);
+//         return state;
+//       }
+//       return [...state, payload];
+
+//     case types.DELETE:
+//       return state.filter(({ id }) => id !== payload);
+
+//     default:
+//       return state;
+//   }
+// };
+
+// const filter = (state = '', { type, payload }) => {
+//   switch (type) {
+//     case 'contacts/changeFilter':
+//       return payload;
+
+//     default:
+//       return state;
+//   }
+// };
