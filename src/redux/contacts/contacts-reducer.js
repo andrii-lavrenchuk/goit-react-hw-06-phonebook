7522567d35bb8e6479707801d9359c8624893e6d
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-02 23:09:12 +0200

    Update contacts-reducer.js

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
-import { toast } from 'react-toastify';
 import actions from '../contacts/contacts-actions';
 
 const items = createReducer([], {
@@ -17,39 +16,3 @@ export default combineReducers({
   items,
   filter,
 });
-// const items = (state = [], { type, payload }) => {
-//   switch (type) {
-//     case types.ADD:
-//       if (payload.name === '' || payload.number === '') {
-//         toast.info('Please fill all fields');
-//         return state;
-//       } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(payload.number)) {
-//         toast.error('Enter the correct  phone number');
-//         return state;
-//       } else if (
-//         state.find(
-//           item => item.name.toLowerCase() === payload.name.toLowerCase(),
-//         )
-//       ) {
-//         toast.info(`Name ${payload.name} is already exsist`);
-//         return state;
-//       }
-//       return [...state, payload];
-
-//     case types.DELETE:
-//       return state.filter(({ id }) => id !== payload);
-
-//     default:
-//       return state;
-//   }
-// };
-
-// const filter = (state = '', { type, payload }) => {
-//   switch (type) {
-//     case 'contacts/changeFilter':
-//       return payload;
-
-//     default:
-//       return state;
-//   }
-// };
