0025be8c5299b97792ef975eca6f6b96bf5806d5
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-02 23:09:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-// import { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-// import store from './redux/store';
-// import shortid from 'shortid';
+
 import Form from './Components/Form/Form';
 import ContactList from './Components/ContactList/ContactList';
 import Filter from './Components/Filter/Filter';
@@ -10,33 +8,14 @@ import Title from './Components/Title/Title';
 import Container from './Components/Container/Container';
 
 export default function App() {
-  // const [contacts, setContacts] = useState(() => {
-  //   return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
-  // });
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-  // const state = store.getState();
-
   return (
     <Container>
       <ToastContainer autoClose={3000} />
       <Title title="Phonebook" />
-
       <Form />
-
       <Title title="Contacts" />
       <Filter />
-      {/* {state.contacts.items.length > 1 && <Filter />} */}
-
       <ContactList />
-      {/* {state.contacts.items.length > 0 ? (
-        <ContactList />
-      ) : (
-        <p>Add your first contact</p>
-      )} */}
     </Container>
   );
 }
